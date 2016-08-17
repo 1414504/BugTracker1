abb3f2ff9662c7cfc6157a09f4ae5324b3cf8858
1414504 <1414504@rgu.ac.uk>
2016-08-17 18:33:02 +0100

    ss

--- addBugs.php
+++ addBugs.php
@@ -5,7 +5,7 @@ if (isset($_POST['save_bug'])) {
     $category = $_POST['bug_category'];
     $summary = $_POST['bug_summary'];
 
-    $con = new mysqli("localhost", "root", "", "bugtracker");
+    $con = new mysqli("ap-cdbr-azure-east-c.cloudapp.net", "b1236d751c9714", "41e55854", "acsm_dd5dc4ba52b59d7");
 
     if (mysqli_errno($con)) {
         die(mysqli_errno($con) . " Failed to connect database.");
--- showBugs.php
+++ showBugs.php
@@ -24,7 +24,7 @@
                 <div class="col-md-12 border min-height">
                     
                     <?php
-                            $con = new mysqli("localhost", "root", "", "bugtracker");
+                            $con = new mysqli("ap-cdbr-azure-east-c.cloudapp.net", "b1236d751c9714", "41e55854", "acsm_dd5dc4ba52b59d7");
                             if(mysqli_errno($con))
                             {
                                 die(mysqli_error($con)." error connectiion failed.");
